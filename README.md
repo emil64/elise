cdb819bba0e10f7291dd8d8113831f779d241e0d
emil <emilcentiu@gmail.com>
2018-12-26 00:36:25 +0200

    done permissions. throws warnings

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # elise
 Tetris infected with a ransomware
+
+http://pubs.opengroup.org/onlinepubs/7990989775/xsh/dirent.h.htmls
+http://www.grymoire.com/Unix/Inodes.html
--- elise.c
+++ elise.c
@@ -1,6 +1,9 @@
 #include <sys/socket.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(){
      setbuf(stdout, NULL);
@@ -14,11 +17,35 @@ int main(){
         printf("Could not open current directory" );
         return 0;
     }
-
     // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
     // for readdir()
-    while ((de = readdir(dr)) != NULL)
-            printf("%s\n", de->d_name);
+    while ((de = readdir(dr)) != NULL){
+            printf("%s : %lu \n", de->d_name, de->d_ino);
+            struct stat fileStat;
+            if(stat(de->d_name,&fileStat) < 0)
+                return 1;
+
+            printf("\n\nInformation for %s\n",de->d_name);
+            printf("---------------------------\n");
+            printf("File Size: \t\t%d bytes\n",fileStat.st_size);
+            printf("Number of Links: \t%d\n",fileStat.st_nlink);
+            printf("File inode: \t\t%d\n",fileStat.st_ino);
+
+            printf("File Permissions: \t");
+            printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+            printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+            printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+            printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+            printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+            printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+            printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+            printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+            printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+            printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+            printf("\n\n");
+
+            printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
+    }
 
     closedir(dr);
 }
